a98defd4eb8a1150713cb25a3060abd3e348aba6
Aleksey.Ozhigov <AlexBurnes@gmail.com>
2025-04-03 19:07:29 +0800

    chore(license): v0.1.1, forget add file LICENSE, do not include degree.cmake by default

--- modules.cmake
+++ modules.cmake
@@ -31,8 +31,5 @@ lr("Install dir: ${CMAKE_INSTALL_PREFIX}")
 include(out_of_source_dir)
 out_of_source_dir("${CMAKE_PROJECT_NAME} requires an out of source build.\n Try:\n\tinit: cmake -B.build -H.\n\tmake: cmake --build .build\n\tinstall: cmake --build .build --target install\n")
 
-include(degree)
-define_parallel_degree()
-
 lr("${Blue}Building project ${BoldWhite}${CMAKE_PROJECT_NAME} ${PROJECT_VERSION} ${Green}${CMAKE_BUILD_TYPE} ${White} Degree ${Bold}${CMAKE_BUILD_PARALLEL_LEVEL}${Reset}")
 
